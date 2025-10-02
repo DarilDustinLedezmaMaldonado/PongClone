9fcea24393f0d0bf8d1f5ffd2536b1b230ccde81
JUSZAYN <victoralejandromollohuarita@gmail.com>
2025-10-02 15:38:07 -0400

    Creando la Scene principal

---
+++ Assets/01_Scripts/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Paddle : MonoBehaviour
+{
+    public float speed = 12f;
+    public bool isPlayer1 = true; // W/S (true) o ↑/↓ (false)
+
+    Rigidbody2D rb;
+    float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            if (Input.GetKey(KeyCode.W)) moveInput = 1f;
+            else if (Input.GetKey(KeyCode.S)) moveInput = -1f;
+            else moveInput = 0f;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.UpArrow)) moveInput = 1f;
+            else if (Input.GetKey(KeyCode.DownArrow)) moveInput = -1f;
+            else moveInput = 0f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0f, moveInput * speed);
+    }
+}
